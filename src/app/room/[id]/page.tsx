2641862be681168b6c67fd0adee80c35ffb301b5
Aaron <xiaofengliu1992@hotmail.com>
2025-02-07 22:41:11 -0500

    remove useRouter in server component

--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -1,13 +1,3 @@
-import { useRouter } from "next/router";
-
 export default function Event() {
-  const router = useRouter();
-  const { id } = router.query;
-
-  return (
-    <div>
-      <h1>Room {id}</h1>
-      <p>Welcome to Room {id}</p>
-    </div>
-  );
+  return <div></div>;
 }
